8726f7b7528b3dba1bce922333b014f5da7f4985
shiimaxx <yoshima@shiimaxx.com>
2018-04-28 23:36:38 +0900

    Initial setup

---
+++ cli.go
@@ -0,0 +1,21 @@
+package main
+
+import "io"
+
+// Exit codes are int values that represent an exit code for a particular error.
+const (
+	ExitCodeOK    int = 0
+	ExitCodeError int = 1 + iota
+)
+
+// CLI is the command line object
+type CLI struct {
+	// outStream and errStream are the stdout and stderr
+	// to write message from the CLI.
+	outStream, errStream io.Writer
+}
+
+// Run invokes the CLI with the given arguments.
+func (c *CLI) Run(args []string) int {
+	return ExitCodeOK
+}
