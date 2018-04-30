e0e010c02c4842ef2418f46e4952ae423a75fb35
shiimaxx <yoshima@shiimaxx.com>
2018-04-30 18:00:39 +0900

    Add feature for print some words and input from stdin

--- cli.go
+++ cli.go
@@ -1,12 +1,30 @@
 package main
 
-import "io"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+)
 
 // Exit codes are int values that represent an exit code for a particular error.
 const (
 	ExitCodeOK    int = 0
 	ExitCodeError int = 1 + iota
 )
+
+var words = []string{
+	"wash",
+	"ill",
+	"ten",
+	"boil",
+	"dynamic",
+	"smiling",
+	"play",
+	"insidious",
+	"reduce",
+	"preserve",
+}
 
 // CLI is the command line object
 type CLI struct {
@@ -15,7 +33,36 @@ type CLI struct {
 	outStream, errStream io.Writer
 }
 
+func input(r io.Reader) <-chan string {
+	ch := make(chan string)
+
+	go func() {
+		s := bufio.NewScanner(r)
+		for s.Scan() {
+			ch <- s.Text()
+		}
+		close(ch)
+	}()
+	return ch
+}
+
 // Run invokes the CLI with the given arguments.
 func (c *CLI) Run(args []string) int {
+	ch := input(os.Stdin)
+	for _, word := range words {
+		fmt.Println(word)
+		fmt.Print(">")
+
+		if v, ok := <-ch; ok {
+			if word == v {
+				fmt.Println("ok")
+			} else {
+				fmt.Println("ng")
+			}
+		} else {
+			break
+		}
+	}
+
 	return ExitCodeOK
 }
