1efe8c2605951eaa638d11212c21c0b451b4143d
shiimaxx <yoshima@shiimaxx.com>
2018-05-02 19:23:34 +0900

    Move core logic to game package

--- cli.go
+++ cli.go
@@ -5,10 +5,9 @@ import (
 	"flag"
 	"fmt"
 	"io"
-	"os"
 	"time"
 
-	"github.com/Pallinder/go-randomdata"
+	"github.com/shiimaxx/typing-game/game"
 )
 
 // Exit codes are int values that represent an exit code for a particular error.
@@ -49,39 +48,9 @@ func (c *CLI) Run(args []string) int {
 		return ExitCodeError
 	}
 
-	ch := input(os.Stdin)
-	var okCount int
+	g := game.NewGame(time.Duration(timeout))
+	questionCount, okCount := g.Run()
 
-	timeoutCh := make(chan struct{})
-	go func() {
-		time.Sleep(time.Duration(timeout) * time.Second)
-		timeoutCh <- struct{}{}
-	}()
-
-	count := 1
-QUESTION_LOOP:
-	for ; ; count++ {
-		word := randomdata.Adjective()
-		fmt.Printf("question %d: %s\n", count, word)
-		fmt.Print("> ")
-
-		select {
-		case v, ok := <-ch:
-			if ok {
-				if word == v {
-					okCount++
-				} else {
-					fmt.Println("ng")
-				}
-			} else {
-				break QUESTION_LOOP
-			}
-		case <-timeoutCh:
-			fmt.Print("Timeup\n\n")
-			break QUESTION_LOOP
-		}
-	}
-	fmt.Fprintf(c.outStream, "result: %d/%d\n", okCount, count)
-
+	fmt.Fprintf(c.outStream, "result: %d/%d\n", okCount, questionCount)
 	return ExitCodeOK
 }
---
+++ game/game.go
@@ -0,0 +1,72 @@
+package game
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"time"
+
+	randomdata "github.com/Pallinder/go-randomdata"
+)
+
+type Game struct {
+	Timeout time.Duration
+}
+
+// NewGame constractor for Game
+func NewGame(timeout time.Duration) *Game {
+	g := new(Game)
+	g.Timeout = timeout
+	return g
+}
+
+func input(r io.Reader) <-chan string {
+	ch := make(chan string)
+
+	go func() {
+		s := bufio.NewScanner(r)
+		for s.Scan() {
+			ch <- s.Text()
+		}
+		close(ch)
+	}()
+	return ch
+}
+
+// Run run game
+func (g *Game) Run() (int, int) {
+	timeoutCh := make(chan struct{})
+	go func() {
+		time.Sleep(time.Duration(g.Timeout) * time.Second)
+		timeoutCh <- struct{}{}
+	}()
+
+	ch := input(os.Stdin)
+	var okCount int
+
+	questionCount := 1
+QUESTION_LOOP:
+	for ; ; questionCount++ {
+		word := randomdata.Adjective()
+		fmt.Printf("question %d: %s\n", questionCount, word)
+		fmt.Print("> ")
+
+		select {
+		case v, ok := <-ch:
+			if ok {
+				if word == v {
+					okCount++
+				} else {
+					fmt.Println("ng")
+				}
+			} else {
+				break QUESTION_LOOP
+			}
+		case <-timeoutCh:
+			fmt.Print("Timeup\n\n")
+			break QUESTION_LOOP
+		}
+	}
+	return questionCount, okCount
+}
